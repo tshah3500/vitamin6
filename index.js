c67f9689463b0e0d2e7a9b0f6d26cdce815dda2c
Tarun Shah <tarun@tarunshah.com>
2025-04-03 20:49:18 -0700

    finished

--- index.js
+++ index.js
@@ -8,7 +8,26 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+let todos = [];
 
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+
+app.post('/todos', (req, res) => {
+  const { task } = req.body;
+  const newTodo = { id: todos.length + 1, task };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+
+app.delete('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    todos = todos.filter((t) => t.id !== parseInt(id));
+    res.status(204).send();
+});
   
 
 
